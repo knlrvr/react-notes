b1342f6be2c3c0090d47d4597aef0c35d1c36386
kane lariviere <91632194+knlrvr@users.noreply.github.com>
2023-03-14 20:51:34 -0400

    Update NoteItem.jsx

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NoteItem = ({note}) => {  
+  var colors = [
+    "#c897ce", 
+    "#eb8890", 
+    "#7facde", 
+    "#92d59a", 
+    "#e3d586",
+    "#d26f6f",
+    "#918ed4",
+    "#88cf8e",
+    "#e5e87d",
+    "#dea36f",
+  ];
+
+  var bgColor = document.getElementsByClassName('note');
+  var uniqueColorIndex = 0;
+  for(var i = 0; i < bgColor.length; i++) {
+    uniqueColorIndex = Math.floor(Math.random() * (colors.length + 1)) 
+    bgColor[i].style['background-color'] = colors[uniqueColorIndex] };
 
   return (
     <Link to={`/view/${note.id}`} className='note'>
